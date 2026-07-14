ad1f6ea2d912c6fc4262d638c9345b99bf62feeb
sangita026 <sandipjana677@gmail.com>
2026-07-14 14:55:40 +0530

    Unit 1

---
+++ pro5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class ScannerExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Roll No: ");
+        int roll = sc.nextInt();
+
+        System.out.print("Enter Name: ");
+        String name = sc.next();
+
+        System.out.print("Enter Percentage: ");
+        float per = sc.nextFloat();
+
+        System.out.println("\nStudent Details");
+        System.out.println("Roll No: " + roll);
+        System.out.println("Name: " + name);
+        System.out.println("Percentage: " + per);
+
+        sc.close();
+    }
+}
